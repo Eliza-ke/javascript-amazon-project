f32966ffa31ca6c8fdcb2938006c12cf88232f47
Eliza-ke <venus.eehs@gmail.com>
2024-08-11 23:00:26 +0630

    make localStorageKey and loadFromStorage private

--- data/cart-class.js
+++ data/cart-class.js
@@ -2,18 +2,18 @@
 // constructor = lets us run setup code after creating an object
 
 class Cart {
-    cartItems;
-    localStorageKey;
+    cartItems; 
+    #localStorageKey; // make private property
 
     // run this object automatically. should not return anything within constructor
     constructor(localStorageKey) {
-        this.localStorageKey = localStorageKey;
-        this.loadFromStorage();
+        this.#localStorageKey = localStorageKey;
+        this.#loadFromStorage();
     }
 
 
-    loadFromStorage() {
-        this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)) || [{
+    #loadFromStorage() {
+        this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey)) || [{
             productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
             quantity: 2,
             deliveryOptionId: '1',
@@ -25,7 +25,7 @@ class Cart {
     };
 
     saveToStorage(){
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems)); // we don't need to paramter for cart because cart is declared above all the codes.
+        localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems)); // we don't need to paramter for cart because cart is declared above all the codes.
     };
 
     addToCart(productId) {
@@ -93,8 +93,7 @@ class Cart {
 const cart = new Cart('cart-oop');
 const businessCart = new Cart('cart-business');
 
-cart.loadFromStorage();
-businessCart.loadFromStorage();
+// cart.#localStorageKey = 'test'; // error
 
 console.log(cart);
 console.log(businessCart);
